24bc456b4d1eab94aa797e9052228e8e39508058
Evan <jpayne0061@gmail.com>
2017-08-01 20:49:36 -0400

    updated MDF file and switched server to localDB instead of sqlExpress

--- TrafficStops/Controllers/StopVarApiController.cs
+++ TrafficStops/Controllers/StopVarApiController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
@@ -19,75 +20,6 @@ namespace TrafficStops.Controllers
             _context = new ApplicationDbContext();
         }
 
-        //[HttpGet]
-        //public List<double> GetStats([FromUri]StopVars query)
-        //{
-        //    query.SetToEmptyString();
-
-        //    List<double> stats = new List<double>();
-
-        //    var allStops = _context.StopEntries.Where(s => s.DriverAgeRange != null)
-        //                                        .Where(s => s.DriverGender != null)
-        //                                        .Where(s => s.DriverRace != null)
-        //                                        .Where(s => s.OfficerGender != null)
-        //                                        .Where(s => s.OfficerRace != null)
-        //                                        .Where(s => s.OfficerAgeRange != null)
-        //                                        .Where(s => s.ActivityResults != null)
-        //                                        .Where(s => s.DriverAgeRange != "")
-        //                                        .Where(s => s.DriverGender != "")
-        //                                        .Where(s => s.DriverRace != "")
-        //                                        .Where(s => s.OfficerGender != "")
-        //                                        .Where(s => s.OfficerRace != "")
-        //                                        .Where(s => s.OfficerAgeRange != "")
-        //                                        .Where(s => s.ActivityResults != "")
-        //                                        .ToList();
-
-
-
-
-        //    var queryCountWandC = allStops.Where(s => s.DriverGender.Contains(query.GenderDriver))
-        //                                        .Where(s => s.OfficerGender.Contains(query.GenderOfficer))
-        //                                        .Where(s => s.DriverRace.Contains(query.RaceDriver))
-        //                                        .Where(s => s.OfficerRace.Contains(query.RaceOfficer))
-        //                                        .Where(s => s.DriverAgeRange.Contains(query.AgeDriver))
-        //                                        .Where(s => s.OfficerAgeRange.Contains(query.AgeOfficer))
-        //                                        .Where(s => s.ActivityResults.Contains(""))
-        //                                        .ToList()
-        //                                        ;
-
-
-        //    var demoCount = queryCountWandC.Count();
-        //    var resultCount = queryCountWandC.Where(s => s.ActivityResults.Contains(query.Result)).Count();
-
-
-        //    double ratio = (double)resultCount / (double)demoCount;
-
-
-        //    var compareResultCountC = allStops.Where(s => s.ActivityResults.Contains("")).Count();
-
-
-        //    //var compareResultCountW = allStops.Where(s => s.ActivityResults.Contains("WARNING")).Count();
-
-        //    //var compareResultCountC = allStops.Where(s => s.ActivityResults.Contains("CITATION ISSUED")).Count();
-
-        //    //var check1 = allStops.Where(s => s.DriverGender.Contains(query.GenderDriver)).ToList();
-        //    //var check2 = allStops.Where(s => s.OfficerGender.Contains(query.GenderOfficer)).ToList();
-
-        //    //var check3 = allStops.Where(s => s.DriverRace.Contains(query.RaceDriver)).ToList();
-        //    //var check4 = allStops.Where(s => s.OfficerRace.Contains(query.RaceOfficer)).ToList();
-
-        //    //var check5 = allStops.Where(s => s.DriverAgeRange.Contains(query.AgeDriver)).ToList();
-        //    //var check6 = allStops.Where(s => s.OfficerAgeRange.Contains(query.AgeOfficer)).ToList();
-
-        //    var rounded = Math.Round(ratio, 2);
-
-
-        //    stats.Add(Math.Round((rounded * 100), 2));
-        //    stats.Add(demoCount);
-        //    stats.Add(resultCount);
-
-        //    return stats;
-        //}
 
         [HttpGet]
         public List<double> GetStats([FromUri] StopVars query)
@@ -101,7 +33,7 @@ namespace TrafficStops.Controllers
 
             using (SqlConnection conn = new SqlConnection())
             {
-                conn.ConnectionString = "Server=JESS_COMPUTER\\SQLEXPRESS;Database=firstDB;Trusted_Connection=true";
+                conn.ConnectionString = ConfigurationManager.ConnectionStrings["firstDBString"].ToString();
                 conn.Open();
 
                 SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM dbo.LMPD_STOPS WHERE OfficerRace LIKE @officerRace AND OfficerAgeRange LIKE @officerAgeRange AND OfficerGender LIKE @officerGender AND DriverRace LIKE @driverRace AND DriverGender LIKE @driverGender AND DriverAgeRange LIKE @driverAgeRange", conn);
